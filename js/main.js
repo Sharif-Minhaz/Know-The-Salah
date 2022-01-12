eed80e16127c31a151c0c5c6c33466e11716f2fd
Minhaz <sharif35-3001@diu.edu.bd>
2022-01-12 17:27:49 +0600

    basic responsive task completed

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// let toTopbtn = document.getElementsByClassName("goTop")[0];
+
+// let pin = document.getElementById("pin");
+// let startingPoint = pin.offsetTop;
+// function backTotop() {
+// 	if (window.pageYOffset <= startingPoint) {
+// 		toTopbtn.classList.add("goTop-hidden");
+
+// 		toTopbtn.classList.remove("goTop");
+// 	} else {
+// 		toTopbtn.classList.add("goTop");
+// 		toTopbtn.classList.remove("goTop-hidden");
+// 	}
+// }
+//back to top
+// window.onscroll = function () {
+// 	backTotop();
+// };
+
+let sidebar = document.getElementsByClassName("sidebar")[0];
+// let triggered = docu
+function changeSidebar(e) {
+	e.classList.toggle("d-none");
+	if (e.classList.contains("menu-reveal")) {
+		e.nextElementSibling.classList.toggle("d-none");
+		sidebar.classList.toggle("fade-sidebar")
+	} else {
+		e.previousElementSibling.classList.toggle("d-none");
+		sidebar.classList.toggle("fade-sidebar");
+	}
+}
