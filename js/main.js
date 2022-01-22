b6e634b8db67c52f6f12fb7e4ff16194d0e7a072
Minhaz <sharif35-3001@diu.edu.bd>
2022-01-22 21:59:39 +0600

    nav and other small bug fixed

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 let sidebar = document.getElementsByClassName("sidebar")[0];
-// let triggered = docu
 function changeSidebar(e) {
 	e.classList.toggle("d-none");
 	if (e.classList.contains("menu-reveal")) {
 		e.nextElementSibling.classList.toggle("d-none");
-		sidebar.classList.toggle("fade-sidebar")
+		sidebar.classList.toggle("fade-sidebar");
 	} else {
 		e.previousElementSibling.classList.toggle("d-none");
 		sidebar.classList.toggle("fade-sidebar");
@@ -12,9 +11,9 @@ function changeSidebar(e) {
 }
 
 let toTopbtn = document.getElementsByClassName("goTop")[0];
-
 let pin = document.getElementById("pin");
 let startingPoint = pin.offsetTop;
+
 function backTotop() {
 	if (window.pageYOffset <= startingPoint) {
 		toTopbtn.classList.add("goTop-hidden");
@@ -26,8 +25,22 @@ function backTotop() {
 	}
 }
 
+// sticky navbar
+let navbar = document.getElementsByClassName("main-nav")[0];
+let sticky = navbar.offsetTop;
+
+function stickyAction() {
+	if (window.pageYOffset >= sticky) {
+		navbar.classList.add("sticky");
+		sidebar.classList.add("sidebar-move-up");
+	} else {
+		navbar.classList.remove("sticky");
+		sidebar.classList.remove("sidebar-move-up");
+	}
+}
+
 // back to top
 window.onscroll = function () {
 	backTotop();
+	stickyAction();
 };
-
